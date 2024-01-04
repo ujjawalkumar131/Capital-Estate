5e3906d953550517048d30a96b421a0083e21448
Ujjawal Kumar <badboyprocoder@gmail.com>
2024-01-05 00:51:01 +0530

    added express and connected MongoDB

---
+++ api/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = process.env.PORT
+
+
+//jab bhi db connect karo => try chatch is necessary and async await is req 
+
+//mongoose.connect(`${MONGO_URL}`); error dega 
+
+
+async function check() {
+    try {
+        const client = await mongoose.connect(process.env.MONGO_URL)
+        console.log("DB connected")
+        
+    } catch(err) {
+        console.log("Database connection error",err);
+    }
+}
+
+check()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Why you are looking here?')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
