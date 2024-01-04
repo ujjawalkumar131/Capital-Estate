604430dafe144e580d98729ecf652708ece531dd
Ujjawal Kumar <badboyprocoder@gmail.com>
2024-01-04 20:01:58 +0530

    added the header and updae the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import {BrowserRouter,Routes , Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Header from './components/Header';
+//import Navbar from './pages/Navbar';
 const App = () => {
+
   return (
-    <div className =''
-    >App</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/" element ={<Home/>} /> 
+    <Route path="/sign-in" element ={<Signin/>} />  
+    <Route path="/sign-up" element ={<Signup/>} />  
+    <Route path="/about" element ={<About/>} />  
+    <Route path="/profile" element ={<Profile/>} />  
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
-export default App
+export default App;
+
+
+
+
